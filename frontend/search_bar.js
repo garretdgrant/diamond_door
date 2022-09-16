5dacdc6e901d31ba6c4e02a64dd963abd2db26fd
Garret G <g.grant92@gmail.com>
2022-09-16 15:19:23 -0700

    heroku deployed

---
+++ frontend/search_bar.js
@@ -0,0 +1,9 @@
+// Notes
+//'diegochavez github ecommerce'
+// establish a route
+// create a custom route under the searched item
+// collection do 
+//     get search/:query to company#search, as search
+
+//controller search
+
