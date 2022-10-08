2a2cacc6cfba998085e1c8965bd06ce6f5ab0873
Garret G <g.grant92@gmail.com>
2022-10-08 10:45:38 -0700

    fix migrate

--- db/migrate/20221008165949_edit_users.rb
+++ db/migrate/20221008165949_edit_users.rb
@@ -1,6 +1,6 @@
 class EditUsers < ActiveRecord::Migration[7.0]
   def change
-    rename_column :users, :first_name, :first_name
-    rename_column :users, :last_name, :last_name
+    rename_column :users, :f_name, :first_name
+    rename_column :users, :l_name, :last_name
   end
 end
