7961921611deb842cb0c3a348b5ed53e30f0f167
Garret G <g.grant92@gmail.com>
2022-10-10 16:22:16 -0700

    heroku

--- db/migrate/20220903203508_create_users.rb
+++ db/migrate/20220903203508_create_users.rb
@@ -2,8 +2,8 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :email, null: false, index: {unique: true}
-      t.string :first_name, null: false
-      t.string :last_name , null: false
+      t.string :f_name, null: false
+      t.string :l_name , null: false
       t.string :website
       t.bigint :phone
       t.text :about_me
