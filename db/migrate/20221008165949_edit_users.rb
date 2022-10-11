cd8171f0c36951cdb3e4885971a16e332beb3be7
Garret G <g.grant92@gmail.com>
2022-10-10 17:11:35 -0700

    heroku

--- db/migrate/20221008165949_edit_users.rb
+++ db/migrate/20221008165949_edit_users.rb
@@ -1,6 +1,6 @@
 class EditUsers < ActiveRecord::Migration[7.0]
   def change
-    # rename_column :users, :f_name, :first_name
+    rename_column :users, :f_name, :first_name
     rename_column :users, :l_name, :last_name
   end
 end
